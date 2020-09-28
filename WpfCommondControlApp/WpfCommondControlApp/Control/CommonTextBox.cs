d19810dd35d387da0a0d57a6bd5d3e6ea7b5e6c5
liufuwentao <247334012@qq.com>
2020-09-28 16:11:23 +0800

    dev;增加描述

--- WpfCommondControlApp/WpfCommondControlApp/Control/CommonTextBox.cs
+++ WpfCommondControlApp/WpfCommondControlApp/Control/CommonTextBox.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace WpfCommondControlApp.Control
+namespace 代码测试.Control
 {
     /// <summary>
-    /// 描述：用法
-    /// xmlns:input="clr-namespace:System.Windows.Input;assembly=PresentationCore"
-    /*<uc1:CommonTextBox Style="{StaticResource textBox}" input:InputMethod.IsInputMethodEnabled="False" Height="50" Width="100" Background="Black"></uc1:CommonTextBox>*/
+    /// 描述：控制只允许输入数字
+    /// 例;
+    /// InputMethod.IsInputMethodEnabled用于设置不允许输入法输入
+    //xmlns:input="clr-namespace:System.Windows.Input;assembly=PresentationCore"
+    //<TextBox x:Name="tb" Width="100" HorizontalAlignment="Right" Background="Black" Foreground="White"
+    //             Margin="0,149,75,143" PreviewTextInput="tb_PreviewTextInput" InputMethod.IsInputMethodEnabled="False"
+    //             Visibility="Collapsed"/>
     /// 作者：liufuwentao
     /// 时间：2020/7/25 17:57:46
     /// </summary>
@@ -44,7 +48,7 @@ namespace WpfCommondControlApp.Control
             {
                 Console.WriteLine(ex.Message);
             }
-
+            
         }
 
         private void IntegerTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
@@ -53,7 +57,7 @@ namespace WpfCommondControlApp.Control
             {
                 if (!char.IsNumber(e.Text.ElementAt(i)))
                 {
-                    Console.WriteLine("不允许输入" + e.Text.ElementAt(i));
+                    Console.WriteLine("不允许输入"+ e.Text.ElementAt(i));
                     e.Handled = true;
                     return;
                 }
