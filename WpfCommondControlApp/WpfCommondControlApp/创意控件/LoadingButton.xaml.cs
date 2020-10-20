8fd8fee245130ac2fe7709fe95ad9defc218fed5
liufuwentao <247334012@qq.com>
2020-10-20 13:29:06 +0800

    dev:增加创意控件

---
+++ WpfCommondControlApp/WpfCommondControlApp/创意控件/LoadingButton.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 代码测试.创意控件
+{
+    /// <summary>
+    /// LoadingButton.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoadingButton : UserControl
+    {
+        #region 依赖项属性
+        /// <summary>
+        /// 是否处于加载中
+        /// </summary>
+        public bool IsOnLoading
+        {
+            get { return (bool)GetValue(IsOnLoadingProperty); }
+            set { SetValue(IsOnLoadingProperty, value);
+                // 是否处于加载中状态，处于则是显示加载中动画，不处于则显示加载完成动画
+                if (value)
+                {
+                    loading.Visibility = Visibility.Visible;
+                    loadingComplete.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    loading.Visibility = Visibility.Collapsed;
+                    loadingComplete.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        public static readonly DependencyProperty IsOnLoadingProperty =
+            DependencyProperty.Register("IsOnLoading", typeof(bool), typeof(Loading)
+                ,new PropertyMetadata(true,null, IsOnLoadingChange));
+
+        private static object IsOnLoadingChange(DependencyObject d, object baseValue)
+        {
+            bool f = (bool)baseValue;
+            //if (f)
+            //{
+            //    loading.Visibility = Visibility.Visible;
+            //    loadingComplete.Visibility = Visibility.Collapsed;
+            //}
+            //else
+            //{
+            //    loading.Visibility = Visibility.Collapsed;
+            //    loadingComplete.Visibility = Visibility.Visible;
+            //}
+            return baseValue;
+        }
+
+        #endregion
+        public LoadingButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfCommondControlApp/WpfCommondControlApp/创意控件/LoadingComplete.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 代码测试.创意控件
+{
+    /// <summary>
+    /// LoadingComplete.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoadingComplete : UserControl
+    {
+        public LoadingComplete()
+        {
+            InitializeComponent();
+        }
+    }
+}
